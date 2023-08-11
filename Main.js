8e93ba3aeb5106646e6487dafcc59956c78da753
YouLookedAtMyUsername <tali.k.gai@gmail.com>
2023-08-11 14:08:14 -0400

    Near End Until Release!!!!!

--- Main.js
+++ Main.js
@@ -1,11 +1,23 @@
-const URL = "http://localhost:3001/get-universe-by-id/3833133292";
-async function GetPlayers() {
-    const HTTPResponce = await fetch(URL);
+const URL = "https://the-legend-of-the-cats-backend.onrender.com/get-universe-by-id/3833133292";
+const ShortenedURL = "https://the-legend-of-the-cats-backend.onrender.com/get-universe-by-id/";
+async function GetPlayers(ID, Visits, Playing) {
+    const HTTPResponce = await fetch(ShortenedURL+ID);
     const HTTPData = await HTTPResponce.json();
-    const Visits = document.getElementById("GameVisitsValue")
-    const Playing = document.getElementById("PlayersActive")
+    // const Visits = document.getElementById("GameVisitsValue")
+    // const Playing = document.getElementById("PlayersActive")
     Visits.textContent = HTTPData["visits"]
     Playing.textContent = `${HTTPData["playing"]} Players Active`
+    return HTTPData
 }
 
-GetPlayers();
+async function Load() { //Gonna be when we release the 2nd game
+    let TotalVisits = 0
+    let TotalPlayersActive = 0
+    const TheLegendOfTheCat = await GetPlayers("3833133292", document.getElementById("GameVisitsValue"), document.getElementById("PlayersActive")) //Copy And Paste This Line And The 2 Lines After This, Change The Universe ID and the Variable Names And You Have A New Game Added To The Total Visits And Playing(If You Uncomment Them in index.html) 
+    TotalVisits += TheLegendOfTheCat["visits"]
+    TotalPlayersActive += TheLegendOfTheCat["playing"]
+}
+
+Load()
+
+setInterval(Load, 10000) //Time Is Milliseconds
